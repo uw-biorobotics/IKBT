306a652c011688d57784c8fa791c44def8f55fed
blake <blake.hannaford@gmail.com>
2017-12-04 10:11:07 -0800

    deleted several student robots for compactness

--- ikbtfunctions/ik_robots.py
+++ ikbtfunctions/ik_robots.py
@@ -28,9 +28,7 @@ from ikbtbasics.ik_classes import *     # special classes for Inverse kinematics
     
 def robot_params(name):
     pvals = {}   # null for most robots
-    List = ['Puma', 'Chair_Helper', 'Wrist', 'MiniDD', 'Olson13','Stanford',\
-            'Sims11', 'Srisuan11', 'Axtman13', 'Mackler13', 'Minder13', 'Palm13', 'Parkman13', \
-            'Frei13', 'Wachtveitl', 'Bartell', 'DZhang', 'Khat6DOF', 'Chair6DOF']
+    List = ['Puma', 'Chair_Helper', 'Wrist', 'MiniDD', 'Olson13','Stanford', 'Chair6DOF']
     assert (name in List), 'robot_params(): Unknown robot, ' + name + ', Stopping'
     
     if(name == 'Puma'):
