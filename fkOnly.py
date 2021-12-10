5a66e867f61c6b72260e1fdaed19466d3ff86042
Blake Hannaford <blake.hannaford@gmail.com>
2021-12-09 21:44:03 -0800

    new Latex output methods working

--- fkOnly.py
+++ fkOnly.py
@@ -29,20 +29,21 @@ import b3 as b3          # behavior trees
 import ikbtfunctions.helperfunctions as hf
 import ikbtfunctions.output_python as op
 import ikbtfunctions.output_cpp as oc
-from ikbtfunctions.ik_robots import *   # a bunch of robot models: probs to solve
+from   ikbtfunctions.ik_robots import *   # a bunch of robot models: probs to solve
+import ikbtfunctions.outputLatex as ol
 
-from ikbtbasics import *
-from ikbtleaves.assigner_leaf import assigner
-from ikbtleaves.rank_leaf import rank
-from ikbtleaves.algebra_solver import *
-from ikbtleaves.tan_solver import *
-from ikbtleaves.sincos_solver import *
-from ikbtleaves.sinANDcos_solver import *
-from ikbtleaves.x2y2_solver import *
-from ikbtleaves.sub_transform import *
-#from ikbtleaves.sum_transform import *  # replaced by sum_id() + Algebra node.
-from ikbtleaves.sum_id import *      # detect and sub sum-of-angles
-from ikbtleaves.two_eqn_m7 import *
+#from ikbtbasics import *
+#from ikbtleaves.assigner_leaf import assigner
+#from ikbtleaves.rank_leaf import rank
+#from ikbtleaves.algebra_solver import *
+#from ikbtleaves.tan_solver import *
+#from ikbtleaves.sincos_solver import *
+#from ikbtleaves.sinANDcos_solver import *
+#from ikbtleaves.x2y2_solver import *
+#from ikbtleaves.sub_transform import *
+##from ikbtleaves.sum_transform import *  # replaced by sum_id() + Algebra node.
+#from ikbtleaves.sum_id import *      # detect and sub sum-of-angles
+#from ikbtleaves.two_eqn_m7 import *
 
 TEST_DATA_GENERATION = False
 
@@ -136,61 +137,12 @@ if TEST_DATA_GENERATION:
         pickle.dump( [R, unks], pf)
     quit()
 
-output_latex_solution(R,unks, final_groups)
+ol.output_FK_equations(R)
 
+ 
 
-#################################################
-# print out all eqnuations that used to solve variables
-# uncomment for debugging
+#            passed ',ntests,' tests \n\n\n')
 
-print("equations evaluated")
-for one_unk in unks:
-    print(one_unk.symbol)
-    print(one_unk.eqntosolve)
-    print(one_unk.secondeqn)
-    print('\n')
-
-
-#
-#
-#
-################################################################################
-
-# define symbols that appear in solutions
-sp.var('r_11 r_12 r_13 r_21 r_22 r_23 r_31 r_32 r_33 Px Py Pz')
-
-
-assertion_count = 0
-ntests = 1
-
-if(robot == 'Chair_Helper'):
-    fs = 'Chair_Helper   FAIL'
-    for u in unks:
-        print('\n Asserting: ', u.symbol, ' = '),
-        if(u.symbol == d_1):
-            ntests += 1
-            assert(u.nsolutions == 1), fs+' n(d_1)'
-            assertion_count += 1
-            print(str(u.solutions[0]))
-            assert(u.solutions[0] == Pz - l_4*r_33), fs + '  [d_1]'
-            assertion_count += 1
-        if(u.symbol == th_2):
-            ntests += 1
-            assert(u.nsolutions == 2), fs+' n(th_2)'
-            assertion_count += 1
-            print(str(u.solutions[0]) + ', ' + str(u.solutions[1]))
-            assert(u.solutions[0] ==  sp.asin((Px-l_1-l_4*r_13)/l_2) ), fs + ' [th_2a]'
-            assertion_count += 1
-            assert(u.solutions[1] == -sp.asin((Px-l_1-l_4*r_13)/l_2)+sp.pi ), fs + ' [th_2b]'
-            assertion_count += 1
-
-if(assertion_count == 0):
-    print('\n         Warning: \n   No Assertions yet for ' + robot)
-else:
-    string = 'test robot '+robot
-    print('\n\n\n                            ',string,'  PASSES ', assertion_count, 'assertions!')
-    print('                                  passed ',ntests,' tests \n\n\n')
-
-print('End of solution job')
+print('End of Forward Kinematics Computation Job')
 
 
