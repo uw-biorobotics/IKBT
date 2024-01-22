ec4918004796e6324f630746b6ba467e84049580
Blake Hannaford <blake.hannaford@gmail.com>
2024-01-22 08:59:04 -0800

    move alphas to defined constant param for Raven-II

--- ikbtfunctions/ik_robots.py
+++ ikbtfunctions/ik_robots.py
@@ -72,8 +72,8 @@ def robot_params(name):
 
         dh = sp.Matrix([
         [  0,          0,     0,      th_1  ],
-        [ sp.pi*75/180,   0 ,     0,      th_2  ],
-        [ (180-52)*sp.pi/180,      0,    d_3,   sp.pi/2 ],         # testing
+        [ al_1,   0 ,     0,      th_2  ],
+        [ al_2,      0,    d_3,   sp.pi/2 ],         # testing
         [ 0,           0,     ld_2,     th_4  ],
         [ sp.pi/2 ,    0,        0,     th_5  ],
         [ sp.pi/2 ,    ld_3,    0,      th_6  ]
@@ -82,8 +82,10 @@ def robot_params(name):
 
         variables =  [unknown(th_1), unknown(th_2), unknown(d_3), unknown(th_4), unknown(th_5), unknown(th_6)]
 
-        params = [  ld_2, ld_3 ]
-        pvals = {ld_2: -470.0, ld_3: 13.0 }
+        params = [  ld_2, ld_3 , al_1, al_2]
+        #pvals = {ld_2: -470.0, ld_3: 13.0, al_1 : sp.pi*75/180, al_2: (180-52)*sp.pi/180}
+        # use np.pi because these are fixed numerical params.
+        pvals = {ld_2: -470.0, ld_3: 13.0, al_1 : np.pi*75/180, al_2: (180-52)*np.pi/180}
 
 ############################################################33    Issues 3 and 4 tests
     if(name == 'Issue4'):
